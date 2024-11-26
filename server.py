838a5a49e7a29001e31191d52874478d0afb97a1
Prashant <prashant@turingpamphlet.com>
2024-11-26 12:51:41 +0530

    handle exceptions globally logic added

--- server.py
+++ server.py
@@ -61,82 +61,88 @@ app = FastAPI()
 
 @app.post("/answer")
 async def answer_questions(request: Request):
-    resp = requests.get(request.url)
-    # print(resp.text)
-    
-    # read pdf from url and convert to text for embeddings
-    on_fly_mem_obj = io.BytesIO(resp.content)
-    pdf_file = PdfReader(on_fly_mem_obj)
-    
-    page = pdf_file.pages[0]
-    text = page.extract_text()
-
     requestid = ulid.new()
-    collection = memorydb.get_or_create_collection(requestid)
+    try:
+        resp = requests.get(request.url)
+        # print(resp.text)
+        
+        # read pdf from url and convert to text for embeddings
+        on_fly_mem_obj = io.BytesIO(resp.content)
+        pdf_file = PdfReader(on_fly_mem_obj)
+        
+        page = pdf_file.pages[0]
+        text = page.extract_text()
 
-    documents = []
-    documentids = []
-    page_count = 0
-    for page in pdf_file.pages:
-        text = page.extract_text() 
-        # print(text)
-        documents.append(text)
-        documentids.append("page-" + str(page_count))
-        page_count += 1
+        collection = memorydb.get_or_create_collection(requestid)
 
-    tokenized_documents, vocabulary = tokenize_documents(documents)
-    bow_embeddings = vectorize_documents(tokenized_documents, vocabulary)
+        documents = []
+        documentids = []
+        page_count = 0
+        for page in pdf_file.pages:
+            text = page.extract_text() 
+            # print(text)
+            documents.append(text)
+            documentids.append("page-" + str(page_count))
+            page_count += 1
 
-    collection.add(
-        embeddings=bow_embeddings,
-        documents=documents,
-        ids=documentids,
-    )
+        tokenized_documents, vocabulary = tokenize_documents(documents)
+        bow_embeddings = vectorize_documents(tokenized_documents, vocabulary)
 
-    final_responses = []
-
-    for question in request.questions:
-        print("question:", question)
-        # embed question and match for similarity with data of pdf
-        question_response = {
-            "question": question,
-        }
-        tokenized_query, _ = tokenize_documents([question])
-        query_embeddings = vectorize_documents(tokenized_query, vocabulary)
-
-        results = collection.query(
-            query_embeddings=query_embeddings,
+        collection.add(
+            embeddings=bow_embeddings,
+            documents=documents,
+            ids=documentids,
         )
-        # print(results)
 
-        context = ""
-        for document in results['documents']:
-            context += document
+        final_responses = []
 
-        # append context to the question being asked by the user
-        chat_response = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": "Based on this given text: \"{context}\" answer the question: \"{question}\" and give confidence score also of how much you think the answer is correct in the format of json with two keys as 'answer' and 'score'. Don't attach any markdown specific tags. just return raw json as text".format(context=context, question=question)},
-            ]
-        )
-        # print( chat_response)
+        for question in request.questions:
+            print("question:", question)
+            # embed question and match for similarity with data of pdf
+            question_response = {
+                "question": question,
+            }
+            tokenized_query, _ = tokenize_documents([question])
+            query_embeddings = vectorize_documents(tokenized_query, vocabulary)
 
-        # check for confidence score and form final response
-        if len(chat_response.choices) > 0:
-            print("Chat response:", chat_response.choices[0].message.content)
-            choice = json.loads(str(chat_response.choices[0].message.content))
-            if choice["score"] < minimum_confidence_score:
-                question_response["answer"] = "Data Not Available"
+            results = collection.query(
+                query_embeddings=query_embeddings,
+            )
+            # print(results)
+
+            context = ""
+            for document in results['documents']:
+                context += document
+
+            # append context to the question being asked by the user
+            chat_response = client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=[
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": "Based on this given text: \"{context}\" answer the question: \"{question}\" and give confidence score also of how much you think the answer is correct in the format of json with two keys as 'answer' and 'score'. Don't attach any markdown specific tags. just return raw json as text".format(context=context, question=question)},
+                ]
+            )
+            # print( chat_response)
+
+            # check for confidence score and form final response
+            if len(chat_response.choices) > 0:
+                print("Chat response:", chat_response.choices[0].message.content)
+                choice = json.loads(str(chat_response.choices[0].message.content))
+                if choice["score"] < minimum_confidence_score:
+                    question_response["answer"] = "Data Not Available"
+                else:
+                    question_response["answer"] = choice["answer"]
             else:
-                question_response["answer"] = choice["answer"]
-        else:
-            question_response["answer"] = "Data Not Available"
-        final_responses.append(question_response)
-    
-    return {
-        "id": str(requestid),
-        "response": final_responses
-    }
-
+                question_response["answer"] = "Data Not Available"
+            final_responses.append(question_response)
+        
+        return {
+            "id": str(requestid),
+            "response": final_responses
+        }
+    except Exception as e:
+        print("error occurred:", e)
+        return {
+            "id": str(requestid),
+            "response": "something went wrong"
+        }
